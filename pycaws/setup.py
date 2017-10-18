d1775f9db46078a4226c3f09e747b218816e12c9
Zachary Sherman <zsherman@anl.gov>
2017-10-18 14:22:12 -0500

    DEV: Adding setup.py for pycaws subpackages.

---
+++ pycaws/setup.py
@@ -0,0 +1,13 @@
+""" Setup for Py-CAWS Subpackages. """
+
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration
+
+def configuration(parent_package='', top_path=None):
+    """ Configuration of pycaw subpackages. """
+    config = Configuration('pycaws', parent_package, top_path)
+    config.add_subpackage('io')
+    return config
+
+if __name__ == '__main__':
+    setup(**configuration(top_path='').todict())
