819fd9891512b798e88d51e8a9ed60e7bf1b6993
Zachary Sherman <zsherman@anl.gov>
2017-11-16 09:23:36 -0600

    MAINT: Adding import from weather_processing.py

--- pycaws/io/__init__.py
+++ pycaws/io/__init__.py
@@ -6,9 +6,11 @@ Input and output
 Functions to read, process and write stage and weather data.
 
     stage_output
+    weather_output
 
 """
 
 from .stage_processing import stage_output
+from .weather_processing import weather_output
 
 __all__ = [s for s in dir() if not s.startswith('_')]
